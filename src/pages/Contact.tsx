7b4a27e670147e2a2ae5d9f232bd3d1cfcabfd5b
Stewar Marin <stewar1020@gmail.com>
2023-11-08 11:32:58 -0500

    feat: :sparkles: Creating contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,39 @@
-import { Box } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Textarea
+} from '@chakra-ui/react'
+import { useState } from 'react'
 
 export const Contact = (): JSX.Element => {
+  const [name, setName] = useState<string>('')
+
+  console.log(name)
+
   return (
-    <Box bgColor='red.500'>Contact</Box>
+    <Box bgColor='white' maxWidth='600px' display='block' m='0 auto'>
+      <Heading size='lg' textAlign='center' mb={3} textTransform='uppercase'>Contact us</Heading>
+      <FormControl p={2}>
+        <FormLabel htmlFor='name'>Name</FormLabel>
+        <Input type='text' id='name' value={name} onChange={(e) => setName(e.target.value)} />
+
+        <FormLabel htmlFor='lastname'>Lastname</FormLabel>
+        <Input type='text' id='lastname' />
+
+        <FormLabel htmlFor='email'>Email</FormLabel>
+        <Input type='email' id='email' />
+
+        <FormLabel htmlFor='email'>Description</FormLabel>
+        <Textarea size='sm' rounded='lg' />
+
+        <Button type='submit' mt={5} colorScheme='blue' width='100%'>
+          Enviar
+        </Button>
+      </FormControl>
+    </Box>
   )
 }
